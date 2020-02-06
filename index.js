e1807ca4904c3cddd9c558cb0de161dd0a186002
github username <github email address>
2020-02-05 20:57:06 -0700

    Done.

--- index.js
+++ index.js
@@ -1,25 +1,64 @@
 const baseURL = 'https://api.github.com';
-const user = '<YOUR_USERNAME>';
+const user = 'cyantis';
 
 function getToken() {
-  //change to your token to run in browser, but set
-  //back to '' before committing so all tests pass
   return '';
 }
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
-  //use fetch to fork it!
+  fetch(
+    `${baseURL}/repos/${repo}/forks`,
+    {
+      method: 'POST',
+      headers: {
+        Authorization: `token ${getToken()}`
+      }
+    }
+  ).then(res => res.json()).then(json => showResults(json));
 }
 
 function showResults(json) {
-  //use this function to display the results from forking via the API
+  const result = document.createElement('p');
+  result.innerHTML = `<a href="${json.html_url}">${json.html_url}</a>`;
+  document.getElementById('results').appendChild(result);
 }
 
 function createIssue() {
-  //use this function to create an issue based on the values input in index.html
+  const obj = {
+    title: document.getElementById('title').value,
+    body: document.getElementById('body').value
+  }
+
+  fetch(
+    `${baseURL}/repos/${user}/js-ajax-fetch-lab/issues`,
+    {
+      method: 'POST',
+      body: JSON.stringify(obj),
+      headers: {
+        Authorization: `token ${getToken()}`
+      }
+    }
+  ).then(res => getIssues());
 }
 
 function getIssues() {
-  //once an issue is submitted, fetch all open issues to see the issues you are creating
+  fetch(
+    `${baseURL}/repos/${user}/js-ajax-fetch-lab/issues`,
+    {
+      headers: {
+        Authorization: `token ${getToken()}`
+      }
+    }
+  ).then(res => res.json()).then(json => issueLis(json));
+}
+
+function issueLis(data){
+  const result = document.createElement('ul');
+
+  for(let i of data){
+    result.innerHTML += `<li>${i.title}</li>`;
+  }
+
+  document.getElementById('issues').appendChild(result);
 }
